65fdefbf1f137d16a5a9d3c333cb4e5acf57db4d
Rekongstor <rekongstor@mail.ru>
2024-10-07 01:46:05 +0300

    Updated LaunchGame.bat. Needs new registry value from the installer

--- PW_MicroUpdater/LaunchGame.bat
+++ PW_MicroUpdater/LaunchGame.bat
@@ -1,6 +1,6 @@
 @echo off
-set "RegKey=HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Uninstall\{869C346D-5682-4A41-BA14-DE721CA43B24}_is1"
-set "ValueName=InstallLocation"
+set "RegKey=HKEY_CURRENT_USER\Software\Classes\pwclassic\GameDir"
+set "ValueName=path"
 
 set WorkingDir=
 for /f "tokens=2*" %%A in ('reg query "%RegKey%" /v "%ValueName%" 2^>nul ^| findstr /i "%ValueName%"') do (
@@ -8,4 +8,4 @@ for /f "tokens=2*" %%A in ('reg query "%RegKey%" /v "%ValueName%" 2^>nul ^| find
 )
 
 cd /d "%WorkingDir%Bin"
-start PW_Game.exe %1
+call "%WorkingDir%Bin\PW_Game.exe" %1
